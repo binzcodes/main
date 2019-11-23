e83f4631338285bedbe4e4ae2f78bf3bda06e1ec
Ryan Binns <hi@rbi.nz>
2019-11-23 18:45:24 +0800

    perf(error page): use computed error properties
    move to using the computed function too manage unhandled default error messages

--- layouts/error.vue
+++ layouts/error.vue
@@ -5,11 +5,9 @@
         <v-col cols="12" sm="8" md="4">
           <v-card class="mx-auto" max-width="400">
             <v-img class="white--text align-end" height="200px" src="/icon.png">
-              <v-card-title v-text="error.statusCode">500</v-card-title>
+              <v-card-title v-text="error.statusCode" />
             </v-img>
-            <v-card-subtitle v-text="error.message" class="grey--text">
-              An error occured
-            </v-card-subtitle>
+            <v-card-subtitle v-text="error.message" class="pb-0" />
             <v-card-actions>
               <v-btn text color="primary" nuxt to="/">Back</v-btn>
             </v-card-actions>
@@ -22,7 +20,7 @@
 
 <script>
 export default {
-  layout: 'empty',
+  layout: 'default',
   props: {
     error: {
       type: Object,
@@ -30,23 +28,17 @@ export default {
     }
   },
   head() {
-    const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
     return {
-      title
+      title: this.message
     }
   },
-  data() {
-    return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred'
+  computed: {
+    statusCode() {
+      return (this.error && this.error.statusCode) || 500
+    },
+    message() {
+      return this.error.message || 'An error occurred'
     }
   }
 }
 </script>
-
-<style scoped>
-h1 {
-  font-size: 20px;
-}
-</style>
