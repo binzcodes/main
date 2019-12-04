eb295869bcea22f56e1ef0af1aa95848caa5656b
Ryan Binns <hi@rbi.nz>
2019-12-04 18:26:19 +0800

    refactor: change default colours
    replace the rest of vuetify's default colours with my brand colours

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,3 @@
-import colors from 'vuetify/es5/util/colors'
-
 export default {
   mode: 'universal',
   /*
@@ -26,7 +24,7 @@ export default {
   /*
    ** Customize the progress-bar color
    */
-  loading: {},
+  loading: { color: '#184757' },
   /*
    ** Global CSS
    */
@@ -70,14 +68,23 @@ export default {
       options: { customeProperties: true },
       dark: false,
       themes: {
+        light: {
+          primary: '#184757',
+          accent: '#43A1C5',
+          secondary: '#83E0A4',
+          info: '#36A8DA',
+          success: '#2A9D8F',
+          warning: '#E9C46A',
+          error: '#E56140'
+        },
         dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
+          primary: '#43A1C5',
+          accent: '#184757',
+          secondary: '#83E0A4',
+          info: '#36A8DA',
+          success: '#2A9D8F',
+          warning: '#E9C46A',
+          error: '#E56140'
         }
       }
     }
